31d3aa355f6282ef3dd4175c631fdc6be9623a9e
Marcus Bitzl <marcus@bitzl.com>
2014-02-26 23:41:01 +0100

    Added prepare function.

--- latex-twitter.lua
+++ latex-twitter.lua
@@ -1,7 +1,28 @@
+function sanitize(text)
+	local new, times = string.gsub(text, "##", "#")
+	return new
+end
+
+
 function urlencode(char)
     local encoded = string.gsub (char, "([^%w])",
         function(c)
             return string.format("%%%02X", string.byte(c))
         end)
     return encoded
+end
+
+
+function texencode(text)
+	local new, times = string.gsub(text, "%%", "\\%%")
+	return new
+end
+
+
+function prepare(text)
+	local result = text
+	result = sanitize(result)
+	result = urlencode(result)
+	result = texencode(result)
+	return result
 end
--- spec/latex-twitter_spec.lua
+++ spec/latex-twitter_spec.lua
@@ -1,6 +1,17 @@
 require("busted")
 require("latex-twitter")
 
+describe("Sanitize the input from TeX", function()
+    it("should replace ## by #", function()
+        assert.same("#", sanitize("##"))
+    end)
+    it("should not change regular text", function()
+        text = "57ad554d8b504d4f80162831ab69e0fb"
+        assert.same(text, sanitize(text))
+    end)
+end)
+
+
 describe("Encoding of URLs", function()
     it("should encode hashtags", function()
         assert.same("%23", urlencode("#"))
@@ -19,5 +30,28 @@ describe("Encoding of URLs", function()
     end)
     it("should encodes URLs", function ()
         assert.same("http%3A%2F%2Fwww%2Eexample%2Ecom%2Findex%2Ephp%3Fid%3D123%26name%3DTest", urlencode("http://www.example.com/index.php?id=123&name=Test"))
+    end)
+end)
+
+
+describe("Encode the str for TeX", function()
+    it("should replace ## by #", function()
+        assert.same("\\%", texencode("%"))
+    end)
+    it("should not change regular text", function()
+        text = "57ad%2055%204"
+        assert.same("57ad\\%2055\\%204", texencode("57ad%2055%204"))
+    end)
+end)
+
+
+describe("Prepare text (the whole stack)", function ()
+    it("shold not change a regular text", function ()
+        text = "1ebf60d5e753443882cd9e33a4c21c87"
+        assert.same(text, prepare(text))
+    end)
+    it("shold not change a regular text but spaces", function ()
+        assert.same("1ebf6\\%200d\\%205e7534438\\%2082cd9e33a\\%204c21c87",
+                    prepare("1ebf6 0d 5e7534438 82cd9e33a 4c21c87"))
     end)
 end)
